104256ba4e13d73fa4b636ac4caeb44d2933116f
nickolay_kuzmin <nickolay.kuzmin2001@gmail.com>
2024-02-10 16:52:22 +0400

    task-1: Add live time to footer.

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,5 +1,13 @@
 <footer class="footer mt-auto py-3 bg-body-tertiary">
     <div class="container">
-        <span class="text-body-secondary">Powered by Колясик</span>
+        <span class="text-body-secondary">Время по Гринвичу:
+            @php
+                use Carbon\Carbon;
+                print(Carbon::now()->format('H:i'));
+            @endphp
+        </span>
+    </div>
+    <div class="container">
+        <span class="text-body-secondary">Powered by Колясик & Dimas</span>
     </div>
 </footer>
